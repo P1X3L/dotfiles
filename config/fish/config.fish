a7423144bc0f6f1a1e0b9ad3e531398ad8869c15
Paul-Xavier Ceccaldi <pix@backuppro.local>
2019-04-16 14:19:34 +0200

    add config folder to dotfiles

---
+++ config/fish/config.fish
@@ -0,0 +1,9 @@
+set -x LANG en_US.UTF-8
+
+#aliases
+source ~/.aliases
+
+# Base16 Shell
+  if status --is-interactive
+    eval sh /Users/pix/.base16-manager/chriskempson/base16-shell/scripts/base16-oceanicnext.sh
+  end
---
+++ config/fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/Users/pix/.fzf/shell/key-bindings.fish
