6502f301d8a3092362ad8e315fdacfbc4192692c
Paul-Xavier Ceccaldi <pix@wttj.co>
2019-07-09 10:08:06 +0000

    add fisher and z
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,13 @@ set -x LANG en_US.UTF-8
 #aliases
 source ~/.aliases
 
+# install fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 # rust config
 set -g CARGO_HOME $HOME/.asdf/installs/rust/1.34.0
 set -g RUSTUP_HOME $HOME/.rustup
@@ -18,7 +25,3 @@ powerline-config tmux setup
 
 #asdf
 source ~/.asdf/asdf.fish
-# Base16 Shell
-  if status --is-interactive
-    eval sh $HOME/.base16-manager/chriskempson/base16-shell/scripts/base16-oceanicnext.sh
-  end
