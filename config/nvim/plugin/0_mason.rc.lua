aafcb15c20d24c44cc016f2e6a3abdb91b9eab72
Paul-Xavier Ceccaldi <pix@wttj.co>
2023-07-27 10:13:42 +0200

    fix: add more conf to mason plugin
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

--- config/nvim/plugin/0_mason.rc.lua
+++ config/nvim/plugin/0_mason.rc.lua
@@ -4,9 +4,18 @@ local status2, lspconfig = pcall(require, "mason-lspconfig")
 if (not status2) then return end
 
 mason.setup({
-
+  ensure_installed = {
+    "eslint_d",
+  }
 })
 
 lspconfig.setup {
-  automatic_installation = true
+  automatic_installation = true,
+  ensure_installed = {
+    "cssls",
+    "eslint",
+    "elixirls",
+    "lua_ls",
+    "tsserver",
+  },
 }
